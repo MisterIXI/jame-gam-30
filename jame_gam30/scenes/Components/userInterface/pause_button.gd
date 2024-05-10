cb6e839c0b9556ffb42d1db9a90a7636fc611c36
OliPohl <oliver@pohl66.de>
2024-05-11 00:12:36 +0200

    button hover and you can buy stuff now

--- jame_gam30/scenes/Components/userInterface/pause_button.gd
+++ jame_gam30/scenes/Components/userInterface/pause_button.gd
@@ -1,45 +1,13 @@
 extends Button
 
-@export var cost : int = 100
-var _resource_manager
-var costText : RichTextLabel
 
 var _focusing = false
 var _unfocusing = false
 
-@export var _icon : CompressedTexture2D = null
-
 
 func _ready() -> void:
-	_resource_manager = get_node("/root/ResourceManager")
-	_resource_manager.money_changed.connect(_on_money_changed)
-
-	costText = get_node("HBoxContainer/cost")
-	costText.text = str(cost)
-
-	_on_money_changed()
-
-	self.pressed.connect(_on_pressed)
-
 	self.mouse_entered.connect(_on_mouse_entered)
 	self.mouse_exited.connect(_on_mouse_exited)
-
-	if _icon != null:
-		get_node("icon").texture = _icon
-
-
-func _on_money_changed() -> void:
-	if _resource_manager._get_money() < cost:
-		disabled = true
-	else:
-		disabled = false
-
-
-func _on_pressed() -> void:
-	_resource_manager._change_money(-cost)
-	# print("Player bought a sick turret for " + str(cost) + " balloons")
-
-	#robert kann hier eingreifen
 
 
 func _on_mouse_entered() -> void:
