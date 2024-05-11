d3b6b25be6ed35fce15886c748fca00440692a25
OliPohl <oliver@pohl66.de>
2024-05-12 01:45:34 +0200

    random seeded brown tones on tiles

--- jame_gam30/scenes/Components/map/tile.gd
+++ jame_gam30/scenes/Components/map/tile.gd
@@ -1,10 +1,28 @@
+class_name  tile
 extends Node3D
 
 # @export var spawnSpeed = 1.0
+var color: Color = Color(0, 1, 1, 1)
+var mesh : MeshInstance3D
+var material 
 
-# func _ready() -> void:
-# 	self.scale = Vector3(0, 0, 0)
-# 	scale_up_over_time(spawnSpeed)
+func _ready() -> void:
+    mesh = get_child(0)
+    material = mesh.material_override.duplicate()
+    mesh.material_override = material
+    mesh.material_override.albedo_color = color
+
+
+
+
+
+
+
+
+
+
+	# self.scale = Vector3(0, 0, 0)
+	# scale_up_over_time(spawnSpeed)
 
 
 
--- jame_gam30/scenes/Map/map.gd
+++ jame_gam30/scenes/Map/map.gd
@@ -2,12 +2,13 @@ class_name map
 extends Node3D
 
 @export var grid_size: int = 20
-var tile : Node3D
+var _tile : Node3D
 var grid : Array = []
 
 @export var animtionTime : float = 1
 var animationStep : float = 0
 
+@export var colorSeed :int = 1234
 var animation = false
 var timer = 0
 
@@ -15,7 +16,7 @@ var character : CharacterBody3D = null
 func _ready() -> void:
 	character = get_node("boi")
 
-	tile = get_node("tile")
+	_tile = get_node("tile")
 	animation = true
 	animationStep = animtionTime / grid_size * grid_size
 
@@ -27,7 +28,8 @@ func _process(delta: float) -> void:
 		if timer >= animationStep:
 			var x = (grid.size() / grid_size) - grid_size / 2
 			var z = (grid.size() % grid_size) - grid_size / 2
-			_placeTile(x,z, Color(randf(), randf(), randf()))
+
+			_placeTile(x,z, generate_random_green())
 
 			if grid.size() >= grid_size * grid_size:
 				animation = false
@@ -36,7 +38,7 @@ func _process(delta: float) -> void:
 
 
 func _placeTile(x: int, z: int, color: Color) -> void:
-	var newTile = tile.duplicate()
+	var newTile = _tile.duplicate()
 	newTile.set("name", "tile_" + str(x) + "_" + str(z))
 	newTile.set("position", Vector3(x, 0, z))
 	newTile.set("color", color)
@@ -48,3 +50,13 @@ func _placeTile(x: int, z: int, color: Color) -> void:
 func _placeCharacter() -> void:
 	character.position = Vector3(0, 0, 0)
 	character.visible = true
+
+
+func generate_random_green() -> Color:
+	var rng = RandomNumberGenerator.new()
+	rng.seed = colorSeed
+	var r = rng.randf_range(0.7, 0.8)  # Keep red component low
+	var g = rng.randf_range(0.5, 0.6)  # Vary green component
+	var b = rng.randf_range(0.3, 0.4)  # Keep blue component low
+	colorSeed += 1  # Increment the seed for the next call
+	return Color(r, g, b, 1.0)  # Alpha is set to 1.0 for fully opaque color
