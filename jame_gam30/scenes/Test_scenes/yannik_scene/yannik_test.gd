440f59623522b0dd83c67a5c0d4888d655c41a84
Yannik Braendle <yannik.braendle@t-online.de>
2024-05-11 10:13:14 +0200

    add test script to git

---
+++ jame_gam30/scenes/Test_scenes/yannik_scene/yannik_test.gd
@@ -0,0 +1,11 @@
+extends Node
+var mybool = false
+# func _physics_process(delta):
+# 	print("Time: ", Time.get_ticks_msec())
+# 	if not mybool and Time.get_ticks_msec() > 2000:
+# 		mybool = true
+# 		Engine.time_scale = 0.02
+# func _input(event):
+# 	if event is InputEventMouseButton:
+# 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+# 			print("Mouse left button pressed")
