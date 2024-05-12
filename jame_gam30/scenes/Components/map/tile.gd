7404ec63144efbfea55fbe5a0d19bf2ef8f61c5e
OliPohl <oliver@pohl66.de>
2024-05-12 03:39:10 +0200

    fixed

--- jame_gam30/scenes/Components/map/tile.gd
+++ jame_gam30/scenes/Components/map/tile.gd
@@ -1,7 +1,6 @@
 class_name  tile
 extends Node3D
 
-# @export var spawnSpeed = 1.0
 var color: Color = Color(0, 1, 1, 1)
 var mesh : MeshInstance3D
 var material 
@@ -12,28 +11,11 @@ func _ready() -> void:
     mesh.material_override = material
     mesh.material_override.albedo_color = color
 
+    
 
 
-
-
-
-
-
-
-
-	# self.scale = Vector3(0, 0, 0)
-	# scale_up_over_time(spawnSpeed)
-
-
-
-# func scale_up_over_time(duration: float) -> void:
-# 	var elapsed_time = 0.0
-# 	var step =  get_process_delta_time() * 0.5 / duration
-
-# 	while elapsed_time < duration:
-# 		elapsed_time += get_process_delta_time()
-# 		if (self.scale.x <= 0.5):
-# 			self.scale += Vector3(1,1,1) * step
-# 		else:
-# 			self.scale = Vector3(1,1,1) * 0.5
-# 		await get_tree().create_timer(get_process_delta_time()).timeout
+# func _process(_delta: float) -> void:
+#     if (self.position.y < -0.2):
+#         self.position.y += 0.2
+#         if (self.position.y > -0.2):
+#             SoundManager.Play_Sound(SoundManager.soundType.hover, Vector3.ZERO)
--- jame_gam30/scenes/Map/map.gd
+++ jame_gam30/scenes/Map/map.gd
@@ -12,6 +12,12 @@ var animationStep : float = 0
 var animation = false
 var timer = 0
 
+@export var path1 : Path3D
+@export var path2 : Path3D
+
+var pathPoints : Array = []
+var steps = 10
+
 var character : CharacterBody3D = null
 func _ready() -> void:
 	character = get_node("boi")
@@ -19,6 +25,20 @@ func _ready() -> void:
 	_tile = get_node("tile")
 	animation = true
 	animationStep = animtionTime / grid_size * grid_size
+
+	for i in range(path1.curve.point_count):
+		var newPoint = round(path1.curve.get_point_position(i))
+		if (i > 0):
+			var lastPoint = pathPoints[pathPoints.size()-1]
+
+			for j in range(steps):
+				var x = round(lerp(lastPoint.x, newPoint.x, j / steps))
+				var z = round(lerp(lastPoint.z, newPoint.z, j / steps))
+				var vector = Vector3(x, 0, z)
+				if (vector not in pathPoints):
+					pathPoints.append(vector)
+
+		pathPoints.append(newPoint)
 
 
 
@@ -41,22 +61,29 @@ func _placeTile(x: int, z: int, color: Color) -> void:
 	var newTile = _tile.duplicate()
 	newTile.set("name", "tile_" + str(x) + "_" + str(z))
 	newTile.set("position", Vector3(x, 0, z))
+
+	if Vector3(x, 0, z) in pathPoints:
+		color = Color(0, 0, 0, 1.0)
+	
 	newTile.set("color", color)
 	add_child(newTile)
 	newTile.visible = true
 	grid.append(newTile)
+	if (z % 2 == 0):
+		SoundManager.Play_Sound(SoundManager.soundType.hover, Vector3.ZERO)
 
 
 func _placeCharacter() -> void:
 	character.position = Vector3(0, 0, 0)
 	character.visible = true
+	SoundManager.Play_Sound(SoundManager.soundType.enemy_die, Vector3(0, 0, 0))
 
 
 func generate_random_green() -> Color:
 	var rng = RandomNumberGenerator.new()
 	rng.seed = colorSeed
-	var r = rng.randf_range(0.7, 0.8)  # Keep red component low
-	var g = rng.randf_range(0.5, 0.6)  # Vary green component
-	var b = rng.randf_range(0.3, 0.4)  # Keep blue component low
-	colorSeed += 1  # Increment the seed for the next call
-	return Color(r, g, b, 1.0)  # Alpha is set to 1.0 for fully opaque color
+	var r = rng.randf_range(0.7, 0.8)
+	var g = rng.randf_range(0.6, 0.6)
+	var b = rng.randf_range(0.4, 0.5)
+	colorSeed += 1
+	return Color(r, g, b, 1.0)
