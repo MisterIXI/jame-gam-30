ff37b36b33fa2cf0bb8889d2e405c48534a065ca
Yannik Braendle <yannik.braendle@t-online.de>
2024-05-12 03:37:41 +0200

    mortar targeting fake implemented (quadratic bezier)

--- jame_gam30/scenes/Test_scenes/yannik_scene/Bomb.gd
+++ jame_gam30/scenes/Test_scenes/yannik_scene/Bomb.gd
@@ -5,19 +5,55 @@ var velocity = Vector3.ZERO
 @export var explosion_particles: CPUParticles3D
 @export var explosion_area: Area3D
 var damage: float = 0
-func shoot(start_vel: Vector3, damage_: float):
-	velocity = start_vel
-	self.damage = damage_
+var p1: Vector3 = Vector3.ZERO
+var p2: Vector3 = Vector3.ZERO
+var p3: Vector3 = Vector3.ZERO
+var p4: Vector3 = Vector3.ZERO
+var toF: float = 0
+var t: float = 0
+var is_on_bezier: bool = true
+var bezier_time: float = 0
+var last_pos: Vector3 = Vector3.ZERO
+
+func shoot(p1_: Vector3, p2_: Vector3, p3_: Vector3, p4_: Vector3, toF_: float, damage_: float):
+	p1 = p1_
+	p2 = p2_
+	p3 = p3_
+	p4 = p4_
+	damage = damage_
+	toF = toF_
+
 func _physics_process(delta):
-	pass
-	# apply gravity to velocity
-	velocity.y -= gravity * delta
-	# move the mortar
-	translate(velocity * delta)
+	if is_on_bezier:
+		last_pos = global_position
+		bezier_time += delta
+		t = bezier_time / toF
+		var new_pos = p1.cubic_interpolate(p4,p2,p3,t)
+		var newer_pos = cooler_ci()
+		global_position = newer_pos
+		if bezier_time >= toF:
+			is_on_bezier = false
+			bezier_time = 0
+			velocity = (new_pos - last_pos) / delta
+	else: 
+		# if not on bezier, apply gravity
+		velocity.y -= gravity * delta
+		# move the mortar
+		translate(velocity * delta)
+
+func cooler_ci() -> Vector3:
+	var a = p1.lerp(p2, t)
+	var b = p2.lerp(p3, t)
+	var c = p3.lerp(p4, t)
+	var d = a.lerp(b, t)
+	var e = b.lerp(c, t)
+	return d.lerp(e, t)
 
 func _on_area_entered(_area):
+	print("Area name: " + _area.name)
 	# check area around current position for any objects
 	# reparent explosion_particles to the the parent above
+
 	explosion_particles.reparent(get_parent().get_parent(),true)
 	explosion_particles.emitting = true
 	# call queue_free() on the explosion_particles after a delay
---
+++ jame_gam30/scenes/Turrets/mortar_turret.gd
@@ -0,0 +1,64 @@
+
+extends StaticBody3D
+class_name MortarTurret
+
+@export var targeting: TowerTargeting
+@export var tower_base: Node3D
+@export var shot_cd_timer: Timer
+@export var bullet: PackedScene
+@export var bullet_speed: float
+@export var test_target: Node3D
+@export var cannon_node: Node3D
+var active 
+func _ready():
+	pass
+	# get_tree().create_timer(2).timeout.connect(test)
+
+# func test():
+# 	print("KABOOM!")
+# 	# var speed = calculate_shooting_speed(test_target.global_position, cannon_node, 2, 9.8)
+# 	shoot_bomb(2,0)
+
+func _physics_process(_delta):
+	pass
+	if targeting.target:
+		if shot_cd_timer.is_stopped():
+			_on_cd_timer_timeout()
+			shot_cd_timer.start()
+		var target_position = targeting.target.global_position
+		var direction = target_position - global_position
+		direction.y = 0
+		# rotate tower_base towards target, only the y axis
+		tower_base.look_at(tower_base.global_position - direction, Vector3.UP)
+
+func _on_active_changed(is_active:bool):
+	active = is_active
+	if active:
+		shot_cd_timer.start()
+	else:
+		shot_cd_timer.stop()
+
+func _on_cd_timer_timeout():
+	if targeting.target:
+		# calc target in 2 seconds on path
+		var enemy = targeting.target as Enemy
+		var new_progress = enemy.progress + enemy.settings.speed * 2
+		var target_position = enemy.parent_path.curve.sample_baked(new_progress) + enemy.parent_path.global_position
+		target_position += Vector3(randf()*3,0,randf()*3)
+		shoot_bomb(target_position, 2, 1)
+
+	else:
+		shot_cd_timer.stop()
+
+func shoot_bomb(target_pos: Vector3, tof: float, damage: float):
+	var bomb = bullet.instantiate() as Bomb
+	get_parent().add_child(bomb)
+	bomb.global_position = cannon_node.global_position
+	var p1 = cannon_node.global_position
+	var p4 = target_pos
+	var dist = p1.distance_to(p4)
+	var p2 = (p1+(p4-p1)*0.25)
+	p2.y += dist*0.5
+	var p3 = (p1+(p4-p1)*0.75)
+	p3.y += dist*0.5
+	bomb.shoot(p1, p2, p3, p4, tof, damage)
