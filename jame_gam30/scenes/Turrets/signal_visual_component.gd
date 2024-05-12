3a7067ffbc228ff768aa7d61688b2e47cc0510c9
Robert Moegenburg <r.moegenburg@t-online.de>
2024-05-12 22:02:39 +0200

    signal_update

---
+++ jame_gam30/scenes/Turrets/signal_visual_component.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@onready var anim : AnimationPlayer =$AnimationPlayer
+@onready var sprite : Sprite3D = $Sprite3D
+func set_active_object(value : bool):
+	if value:
+		
+		anim.play("active")
+
+	else:
+		
+		anim.play("deactivate")
